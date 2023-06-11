c619632e0ad3e575000d13941b2c42e75ffea33e
AishaOthman <75293286+AishaOthman@users.noreply.github.com>
2023-06-11 11:39:59 +0300

    Add files via upload

--- express/server.ts
+++ express/server.ts
@@ -1,20 +1,58 @@
 import express from 'express';
+import cors from 'cors';
+import  _ from "lodash";
+import neo4j from 'neo4j-driver';
+import bodyParser from 'body-parser';
+import { QueryPayload, IRecipe, ingredient } from 'shared_data';
 
 const app = express();
+app.use(cors());
 const port = 3001;
 
-export interface QueryPayload{
-    foo : string;
-}
-
-app.get("/data", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    const data: QueryPayload = { foo: "bar" }
-    
-    res.json(data);
-  });
+app.use((_req, res, next) => {
   
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    // Continue to next middleware
+    next();
   });
+  app.get("/", (_req, res) => {
+    const responseData: QueryPayload = {
+        payload: _.snakeCase("Server data returned successfully"),
+    };
+  
+    res.json(responseData);
+  });
+  const  driver =neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic("neo4j", "password"))
+  app.get("/neo4j",(req,res)=>{
+   
+    const session = driver.session();
+    try{
+      console.log("starting session!!")
+      session.executeRead(txc=> txc.run('match(n) return n').then((result: { records: any[]; })=> {
+        const recordsName:string[] = result.records.map(r => r.get('n').properties.name);
+        const responseData:QueryPayload = {
+          payload:recordsName
+        }
+        res.json(responseData);
+      }))
+   
+    }catch(e){
+      console.log("Query Failed!");
+      session.close();
+      res.status(500)
+      res.render('error', { error: e })
+    }
+  
+  })
+  
+  
+
+// app.get("/",(req,res)=>{
+
+//   
+//     const data :QueryPayload ={foo : "bar"}
+//     res.json(data);
+// });
+
+app.listen(port,()=>{
+    console.log(' app listening att http://localhost:${port}');
+});
