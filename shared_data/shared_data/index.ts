493bed5bbc4913056a7bde69238527530d473ca0
AishaOthman <75293286+AishaOthman@users.noreply.github.com>
2023-07-12 09:23:25 +0300

    Add files via upload

---
+++ shared_data/shared_data/index.ts
@@ -0,0 +1,22 @@
+export interface QueryPayload {
+    payload: string | string [];
+}
+export interface IIngredient {
+    name: string;
+    amount: string;
+  }
+export interface IRecipe {
+      recipeName: string;
+      description?:string;
+      authorName: string;
+      prepTime:number;
+      cookTime:number;
+      category:string;
+      dietType:string;
+      ratings:number;
+      skilLevel:string;
+      dishType:string;
+      serves:number;
+      ingredients:IIngredient[]
+      instructions:string[];
+}
