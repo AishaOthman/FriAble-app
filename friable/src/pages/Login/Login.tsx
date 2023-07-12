b38070757b4fccfa1b7b1af75fd398bef9843235
AishaOthman <75293286+AishaOthman@users.noreply.github.com>
2023-07-12 09:16:24 +0300

    Add files via upload

--- friable/src/pages/Login/Login.tsx
+++ friable/src/pages/Login/Login.tsx
@@ -1,57 +1,99 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Toolbar } from '@mui/material';
 
-import { Typography, Grid, CssBaseline, Paper, Avatar, TextField, FormControlLabel, Checkbox, Button, Box } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
- //import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        FRIABLE
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
+const theme = createTheme();
 
-//  const useStyles = makeStyles((theme:any) => ({
-//     root: {
-//       height: '100vh',
-//     },
-//     image: {
-//       backgroundImage: 'url(https://source.unsplash.com/random)',
-//       backgroundRepeat: 'no-repeat',
-//       backgroundColor:
-//         theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-//       backgroundSize: 'cover',
-//       backgroundPosition: 'center',
-//     },
-//     paper: {
-//       margin: theme.spacing(8, 4),
-//       display: 'flex',
-//       flexDirection: 'column',
-//       alignItems: 'center',
-//     },
-//     avatar: {
-//       margin: theme.spacing(1),
-//       backgroundColor: theme.palette.secondary.main,
-//     },
-//     form: {
-//       width: '100%', // Fix IE 11 issue.
-//       marginTop: theme.spacing(1),
-//     },
-//     submit: {
-//       margin: theme.spacing(3, 0, 2),
-//     },
-//   }));
-  
-  export default function SignInSide() {
-   // const classes = useStyles();
-  
-    return (
-      <Grid container component="main" >
+export default function SignInSide() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
-        <Grid item xs={false} sm={4} md={7} />
+        < AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="h6" sx={{ flexGrow: 1 }} >
+          Welcom To FriAble <br/><h6>Food recipes app</h6>
+          </Typography>
+        
+          <Box sx={{ marginLeft: 'auto' }}>
+        <Link color="inherit" href="/">Home</Link>
+        <Box sx={{ marginLeft: 2, display: 'inline' }} />
+        <Link color="inherit" href="/SingUp">SingUp</Link>
+     
+      </Box>
+          
+          
+        </Toolbar>
+      </AppBar>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            //https://source.unsplash.com/random
+            //
+            backgroundImage: 'url(https://images.unsplash.com/photo-1540914124281-342587941389?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <div >
-           
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+             Log in
             </Typography>
-            <form  noValidate>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
-                variant="outlined"
                 margin="normal"
                 required
                 fullWidth
@@ -62,7 +104,6 @@ import { Link } from 'react-router-dom';
                 autoFocus
               />
               <TextField
-                variant="outlined"
                 margin="normal"
                 required
                 fullWidth
@@ -77,39 +118,31 @@ import { Link } from 'react-router-dom';
                 label="Remember me"
               />
               <Button
+              href="/Home1"
                 type="submit"
                 fullWidth
                 variant="contained"
-                color="primary"
-                
+                sx={{ mt: 3, mb: 2 }}
               >
                 Sign In
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link to="#">
+                  <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link to="#">
+                  <Link href="./SingUP" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
               </Grid>
-              <Box mt={5}>
-                <Typography variant="body2" color="textSecondary" align="center">
-                  {'Copyright © '}
-                  <Link color="inherit" to="https://mui.com/">
-                    Your Website
-                  </Link>{' '}
-                  {new Date().getFullYear()}
-                  {'.'}
-                </Typography>
-              </Box>
-            </form>
-          </div>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
         </Grid>
       </Grid>
-    );
-  }
+    </ThemeProvider>
+  );
+}
